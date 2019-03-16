68d31abb8a4f6cdc8dc14b5c49229d07fcccff71
James Little <james@ergo.kiwi>
2019-03-17 00:03:31 +1300

    Included crow_all header and added README

---
+++ hello_crow/main.cpp
@@ -0,0 +1,25 @@
+#include "crow_all.h"
+
+using namespace std;
+
+#include <string>
+
+int main(int argc, char *argv[]) {
+    crow::SimpleApp app;
+
+    app.loglevel(crow::LogLevel::Debug);
+
+    CROW_ROUTE(app, "/")
+            ([]() {
+                return "<div><h1>Hello, Crow.</h1></div>";
+            });
+    CROW_ROUTE(app, "/<string>")
+            ([](string name) {
+                return "<div><h1>Hello, " + name + ".</h1></div>";
+            });
+
+    char *port = getenv("PORT");
+    uint16_t iPort = static_cast<uint16_t>(port != NULL ? stoi(port) : 18080);
+    cout << "PORT = " << iPort << "\n";
+    app.port(iPort).multithreaded().run();
+}
