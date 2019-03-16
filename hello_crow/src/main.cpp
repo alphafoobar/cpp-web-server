bf88353fa5de8857ffb1a836e6a8759e31fe3127
James Little <james@ergo.kiwi>
2019-03-17 00:43:42 +1300

    Adding framework for unit testing

--- hello_crow/src/main.cpp
+++ hello_crow/src/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char *argv[]) {
     crow::SimpleApp app;
 
-    app.loglevel(crow::LogLevel::Debug);
+    app.loglevel(crow::LogLevel::Info);
 
     CROW_ROUTE(app, "/")
             ([]() {
